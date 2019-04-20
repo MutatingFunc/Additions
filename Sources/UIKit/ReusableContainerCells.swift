8dfc194b509be276602771cfb0a8070e26ac1b20
James <james.froggatt@me.com>
2019-04-20 14:43:38 +0100

    Made reusable cells reuseIdentifiable, API improvements

--- Sources/UIKit/ReusableContainerCells.swift
+++ Sources/UIKit/ReusableContainerCells.swift
@@ -8,27 +8,51 @@
 #if canImport(UIKit)
 import UIKit
 
-public class TableViewCell<ViewType: UIView>: UITableViewCell {
+public class TableViewCell<ViewType: UIView>: UITableViewCell, ReuseIdentifiable {
 	private var containedView: ViewType!
-	public func setContent(_ view: ViewType) {
-		precondition(self.containedView == nil, "Content should only be set once")
+	public func withInitialContent(_ view: ViewType) -> Self {
+		if self.containedView == nil {
+			return self.withContent(view)
+		}
+		return self
+	}
+	public func withContent(_ view: ViewType) -> Self {
+		if self.containedView === view {
+			return self
+		} else {
+			self.containedView?.removeFromSuperview()
+		}
+		view.removeFromSuperview()
 		self.containedView = view
 		containedView.frame = self.contentView.bounds
 		containedView.translatesAutoresizingMaskIntoConstraints = true
 		containedView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
 		self.contentView.addSubview(containedView)
+		return self
 	}
 }
 
-public class CollectionViewCell<ViewType: UIView>: UICollectionViewCell {
+public class CollectionViewCell<ViewType: UIView>: UICollectionViewCell, ReuseIdentifiable {
 	private var containedView: ViewType!
-	public func setContent(_ view: ViewType) {
-		precondition(self.containedView == nil, "Content should only be set once")
+	public func withInitialContent(_ view: ViewType) -> Self {
+		if self.containedView == nil {
+			return self.withContent(view)
+		}
+		return self
+	}
+	public func withContent(_ view: ViewType) -> Self {
+		if self.containedView === view {
+			return self
+		} else {
+			self.containedView?.removeFromSuperview()
+		}
+		view.removeFromSuperview()
 		self.containedView = view
 		containedView.frame = self.contentView.bounds
 		containedView.translatesAutoresizingMaskIntoConstraints = true
 		containedView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
 		self.contentView.addSubview(containedView)
+		return self
 	}
 }
 #endif
