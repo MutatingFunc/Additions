387b19d82eff06e578ea7ee6953628573e236f3c
James <james.froggatt@me.com>
2019-05-04 20:06:13 +0100

    Added Weak<AnyObject>

--- Sources/Additions/Weak.swift
+++ Sources/Additions/Weak.swift
@@ -6,10 +6,6 @@
 //  Copyright © 2016 James Froggatt. All rights reserved.
 //
 
-/*
-inactive - no valid use found
-*/
-
 ///a weak wrapper around a reference-type
 public struct Weak<Reference: AnyObject> {
 	public weak var target: Reference?
@@ -18,7 +14,15 @@ public struct Weak<Reference: AnyObject> {
 extension Weak: ExpressibleByNilLiteral {
 	public init(nilLiteral: ()) {target = nil}
 }
-extension Weak: Hashable {}
+extension Weak: Hashable {
+	public static func ==(lhs: Weak<Reference>, rhs: Weak<Reference>) -> Bool {
+		return lhs.target === rhs.target
+	}
+	
+	public func hash(into hasher: inout Hasher) {
+		hasher.combine(target.flatMap(ObjectIdentifier.init))
+	}
+}
 extension Weak: CustomDebugStringConvertible {
 	public var debugDescription: String {return target.debugDescription}
 }
