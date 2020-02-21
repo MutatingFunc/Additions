07aabff9c922eeed457745977c0abbd21ccc6477
James Froggatt <james.froggatt@me.com>
2020-02-21 19:36:27 +0000

    Make ActivityIndicator public

--- Sources/Additions/SwiftUI/Views.swift
+++ Sources/Additions/SwiftUI/Views.swift
@@ -10,16 +10,20 @@ import SwiftUI
 import UIKit
 
 @available(iOS 13.0, tvOS 13.0, *)
-struct ActivityIndicator: UIViewRepresentable {
-	
+public struct ActivityIndicator: UIViewRepresentable {
+	var style: UIActivityIndicatorView.Style
 	@Binding var isAnimating: Bool
-	let style: UIActivityIndicatorView.Style
 	
-	func makeUIView(context: UIViewRepresentableContext<ActivityIndicator>) -> UIActivityIndicatorView {
+	public init(style: UIActivityIndicatorView.Style, isAnimating: Binding<Bool> = .constant(false)) {
+		self.style = style
+		self._isAnimating = isAnimating
+	}
+	
+	public func makeUIView(context: UIViewRepresentableContext<ActivityIndicator>) -> UIActivityIndicatorView {
 		return UIActivityIndicatorView(style: style)
 	}
 	
-	func updateUIView(_ uiView: UIActivityIndicatorView, context: UIViewRepresentableContext<ActivityIndicator>) {
+	public func updateUIView(_ uiView: UIActivityIndicatorView, context: UIViewRepresentableContext<ActivityIndicator>) {
 		isAnimating ? uiView.startAnimating() : uiView.stopAnimating()
 	}
 }
